7c9bc86f4aba517b64d2f1a4c1e497a4df6e2a40
Nathaniel Mak <nathanielmak@Nathaniels-MacBook-Air.local>
2025-10-04 16:46:19 -0700

    base html layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect } from 'react';
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div id="title" className="w-full h-full flex justify-center items-center">
+        <h1>Rate the Washroom</h1>
+      </div>
+      <div id='navbar' className="w-full h-full flex justify-center items-center">navbar</div>
+      <div id="map" className="w-full h-full flex justify-center items-center">Map</div>
+      <div id="list" className="w-full h-full flex justify-center items-center">List</div> 
+    </>
+  );
 }
