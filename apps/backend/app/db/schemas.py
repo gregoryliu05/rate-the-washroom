6d537f4b01593da8c97bdeba5eba252663141fd1
Nicholas Tam <nta59@sfu.ca>
2025-10-04 22:05:31 -0700

    [FEAT] added required JSON structure for "Review" api endpoints

--- apps/backend/app/db/schemas.py
+++ apps/backend/app/db/schemas.py
@@ -62,3 +62,52 @@ class WashroomCreate(BaseModel):
         from_attributes = True
 
 
+
+
+### REVIEW ###
+
+# return all but washroom_id since already known
+class ReviewOutByWashroom(BaseModel):
+    id: uuid
+    user_id: uuid
+    rating: int
+    title: str
+    description: str
+    likes : int
+    created_at: datetime
+    updated_at: datetime
+
+# return all but user_id since already known
+class ReviewOutByUser(BaseModel):
+    id: uuid
+    washroom_id: uuid
+    rating: int
+    title: str
+    description: str
+    likes : int
+    created_at: datetime
+    updated_at: datetime
+
+# creating requires all attributes
+class ReviewCreate(BaseModel):
+    id: uuid
+    user_id
+    washroom_id: uuid
+    rating: int
+    title: str
+    description: str
+    likes : int
+    created_at: datetime
+    updated_at: datetime
+
+# 
+class ReviewEdit(BaseModel):
+    id: uuid
+    user_id
+    washroom_id: uuid
+    rating: int
+    title: str
+    description: str
+    updated_at: datetime
+
+
