9aec9f92ed335476b60eba7a8e907f6dd2471efd
Nicholas Tam <nta59@sfu.ca>
2025-10-05 00:10:30 -0700

    [FEAT] Implement review API endpoints for creating, retrieving, updating, and deleting reviews; update schemas for review handling

--- apps/backend/app/db/schemas.py
+++ apps/backend/app/db/schemas.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 from datetime import datetime
-import uuid
+from uuid import UUID
+from typing import Optional
 
 class UserCreate(BaseModel):
-    username: str
+    username: UUID
     email: str
     first_name: str
     last_name: str
@@ -14,7 +15,7 @@ class UserCreate(BaseModel):
 
 
 class UserOut(BaseModel):
-    id: str
+    id: UUID
     username: str
     email: str
     first_name: str
@@ -26,7 +27,7 @@ class UserOut(BaseModel):
 
 
 class WashroomOut(BaseModel):
-    id: str
+    id: UUID
     name: str
     description: str
     address: str
@@ -38,7 +39,7 @@ class WashroomOut(BaseModel):
     opening_hours: dict  # Or Optional[dict] if nullable
     overall_rating: float
     rating_count: int
-    created_by: str  # UUID as string
+    created_by: UUID 
 
     class Config:
         from_attributes = True
@@ -56,7 +57,7 @@ class WashroomCreate(BaseModel):
     long: float
     overall_rating: float
     rating_count: int
-    created_by: str  # UUID as string
+    created_by: UUID  # UUID type
 
     class Config:
         from_attributes = True
@@ -66,48 +67,51 @@ class WashroomCreate(BaseModel):
 
 ### REVIEW ###
 
+
 # return all but washroom_id since already known
 class ReviewOutByWashroom(BaseModel):
-    id: uuid
-    user_id: uuid
+    id: UUID
+    user_id: UUID
     rating: int
     title: str
     description: str
     likes : int
     created_at: datetime
     updated_at: datetime
+
+    class Config:
+        from_attributes = True
 
 # return all but user_id since already known
 class ReviewOutByUser(BaseModel):
-    id: uuid
-    washroom_id: uuid
+    id: UUID
+    washroom_id: UUID
     rating: int
     title: str
     description: str
     likes : int
     created_at: datetime
     updated_at: datetime
+
+    class Config:
+        from_attributes = True
 
 # creating requires all attributes
 class ReviewCreate(BaseModel):
-    id: uuid
-    user_id
-    washroom_id: uuid
+    washroom_id: UUID
+    user_id: UUID
     rating: int
-    title: str
-    description: str
-    likes : int
-    created_at: datetime
-    updated_at: datetime
+    title: Optional[str] = None
+    description: Optional[str] = None
 
-# 
+    class Config:
+        from_attributes = True
+
+
 class ReviewEdit(BaseModel):
-    id: uuid
-    user_id
-    washroom_id: uuid
     rating: int
-    title: str
-    description: str
-    updated_at: datetime
+    title: Optional[str] = None
+    description: Optional[str] = None
 
-
+    class Config:
+        from_attributes = True
