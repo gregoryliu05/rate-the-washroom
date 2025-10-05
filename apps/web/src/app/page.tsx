14496f7e82900498883bf70599b9313da89e692f
Nathaniel Mak <nathanielmak@Nathaniels-MacBook-Air.local>
2025-10-04 17:00:25 -0700

    added navbar to homepage

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import Navbar from './components/navbar';
 import { useEffect } from 'react';
 
 export default function Home() {
@@ -8,7 +8,9 @@ export default function Home() {
       <div id="title" className="w-full h-full flex justify-center items-center">
         <h1>Rate the Washroom</h1>
       </div>
-      <div id='navbar' className="w-full h-full flex justify-center items-center">navbar</div>
+       <div id='navbar' className="w-full h-full flex justify-center items-center">
+         <Navbar />
+       </div>
       <div id="map" className="w-full h-full flex justify-center items-center">Map</div>
       <div id="list" className="w-full h-full flex justify-center items-center">List</div> 
     </>
