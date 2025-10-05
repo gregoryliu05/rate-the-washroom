597ed7d603df512e4f405b2b6f28110bbb2cb455
gliu25 <gregoryliu123@gmail.com>
2025-10-05 02:29:38 -0700

    lol

--- apps/backend/app/api/deps.py
+++ apps/backend/app/api/deps.py
@@ -1,10 +1,11 @@
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_db
+from typing import AsyncGenerator
 
 # ADD Firebase token verification dependency here in the future
 
-async def get_db_session() -> AsyncSession:
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
     async for db in get_db():
         yield db
 
